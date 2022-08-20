826fc27e7e95241fe00eee078fddabf276f81561
alberto <55685868+scaryalberto@users.noreply.github.com>
2022-08-20 18:15:47 +0200

    Update wsgi.py

--- sito_cv/wsgi.py
+++ sito_cv/wsgi.py
@@ -14,3 +14,27 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sito_cv.settings')
 
 application = get_wsgi_application()
+
+
+import os 
+import time 
+import traceback 
+import signal 
+import sys 
+ 
+from django.core.wsgi import get_wsgi_application 
+ 
+sys.path.append('/home/admin/django_projects/sito_cv_alb') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/home/admin/django_projects/sito_cv_alb/env/lib/python3.7/site-packages') 
+ 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sito_cv.settings')
+ 
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
