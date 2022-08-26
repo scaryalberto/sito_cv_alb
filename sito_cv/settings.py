caa6b3506904d505507800f2df1b6708b72d7940
root <root@ip-172-26-9-168.eu-central-1.compute.internal>
2022-08-26 21:35:32 +0000

    inserita riga mancante in wsgy.py

--- sito_cv/settings.py
+++ sito_cv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+#(22g4bsfo%bdqpny&+6ivguk2-n!3$%h93r7_m*r&ugqmd-o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
--- sito_cv/wsgi.py
+++ sito_cv/wsgi.py
@@ -1,40 +1,23 @@
-"""
-WSGI config for sito_cv project.
+import sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+path='/var/www/sito_cv_alb'
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
+if path not in sys.path:
+    sys.path.append(path)
 
+
+
+        
+
+          
 import os
+import sys
+
+sys.path.append('/var/www/sito_cv_alb')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/var/www/sito_cv_alb/sito_cv/egg_cache")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sito_cv.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sito_cv.settings')
-
 application = get_wsgi_application()
 
-
-import os 
-import time 
-import traceback 
-import signal 
-import sys 
- 
-from django.core.wsgi import get_wsgi_application 
- 
-sys.path.append('/home/admin/django_projects/sito_cv_alb') 
-# adjust the Python version in the line below as needed 
-sys.path.append('/home/admin/django_projects/sito_cv_alb/env/lib/python3.7/site-packages') 
- 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sito_cv.settings')
- 
-try: 
-    application = get_wsgi_application() 
-except Exception: 
-    # Error loading applications 
-    if 'mod_wsgi' in sys.modules: 
-        traceback.print_exc() 
-        os.kill(os.getpid(), signal.SIGINT) 
-        time.sleep(2.5) 
