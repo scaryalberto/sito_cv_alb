a60ba465f39d5b5c1324ea5531b4d8800dde02a8
Scaringi <alsg@gft.com>
2022-09-12 17:56:40 +0200

    rimossi commenti inutili

--- cv/views.py
+++ cv/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import BlogArticles
-# Create your views here.
-from datetime import datetime
-
+from .forms import NewArticleForm
 
 def home_page(request):
     return render(request, 'cv/index.html', {})
+
+
 
 def blog_posts(request):
     posts = BlogArticles.objects.all()
@@ -17,11 +17,6 @@ def blog_posts(request):
 def article(request, id):
     post = get_object_or_404(BlogArticles, id=id)
     return render(request, 'cv/article.html', {'post':post})
-
-
-from django.shortcuts import render
-from .forms import NewArticleForm
-from django.contrib import messages #import messages
 
 from django.shortcuts import redirect
 
@@ -34,11 +29,6 @@ def add_article(request):
 
     # check if form data is valid
     if form.is_valid():
-        # save the form data to model
-        #now = datetime.now()
-        #form.data=now.date()
-        messages.success(request,'Data has been submitted')
-
         form.save()
         response = redirect('/blog_posts')
         return response
